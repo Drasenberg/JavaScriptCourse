d390a3ab12e22b2018d9b67b5399cbd6be38c651
Drasenberg <macoel14@gmail.com>
2022-02-25 13:46:50 +0100

    assingment 2 Finished

--- basics-assignment-2-start/assignment.js
+++ basics-assignment-2-start/assignment.js
@@ -1,1 +1,19 @@
 const task3Element = document.getElementById('task-3');
+
+function simplyAlert() {
+  alert('Hi michel');
+}
+
+function alertWithParameter(name) {
+  alert(name);
+}
+
+simplyAlert();
+alertWithParameter('Max');
+
+task3Element.addEventListener('click', simplyAlert);
+
+function helloWorldAndMichell(hello, world, michell) {
+  return hello + ' ' + world + ' and ' + michell;
+}
+alert(helloWorldAndMichell('Hello', 'world', 'Michell'));
