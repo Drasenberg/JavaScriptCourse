6d9a5c762d0c9a2a666caef949f9704eb60dcf78
Drasenberg <macoel14@gmail.com>
2022-06-28 23:51:20 +0200

    End of assignment 5

---
+++ dom-assignment-start/assignment.js
@@ -0,0 +1,15 @@
+// Task 1
+const taskBg = document.querySelector('li');
+taskBg.style.backgroundColor = 'black';
+const taskC = document.getElementById('task-1');
+taskC.style.color = 'white';
+
+// Task 2
+const title = document.querySelector('title');
+title.textContent = 'Assignment - Solved!';
+const head = document.querySelector('head');
+head.querySelector('title').textContent = 'Assignment - Solved!!';
+
+// Task 3
+const h1 = document.querySelector('h1');
+h1.textContent = 'Assignment - Solved!';
