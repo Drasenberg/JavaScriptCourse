4246e170362078f4dd6c730129c201e4307f58c9
Drasenberg <macoel14@gmail.com>
2022-11-23 07:53:28 +0100

    End of async

--- async-01-starting-setup/app.js
+++ async-01-starting-setup/app.js
@@ -1,8 +1,80 @@
 const button = document.querySelector('button');
 const output = document.querySelector('p');
 
-function trackUserHandler() {
-  console.log('Clicked!');
+const getPosition = (opts) => {
+  const promise = new Promise((res, rej) => {
+    navigator.geolocation.getCurrentPosition(
+      (success) => {
+        res(success);
+      },
+      (error) => {
+        rej(error);
+      },
+      opts
+    );
+  });
+  return promise;
+};
+
+const setTimer = (duration) => {
+  const promise = new Promise((reslove, reject) => {
+    setTimeout(() => {
+      reslove('Done!');
+    }, duration);
+  });
+  return promise;
+};
+
+async function trackUserHandler() {
+  // let positionData;
+  let posData;
+  let timerData;
+
+  try {
+    posData = await getPosition();
+    timerData = await setTimer(2000);
+  } catch (error) {
+    console.log(error);
+  }
+
+  console.log(timerData, posData);
+  // .then((posData) => {
+  //   positionData = posData;
+  //   return setTimer(2000);
+  // })
+  // .catch((err) => {
+  //   console.log(err);
+  //   return 'on we go... ';
+  // })
+  // .then((data) => {
+  //   console.log(data, positionData);
+  // });
+  setTimer(1000).then(() => {
+    console.log('Timer is done!');
+  });
+  console.log('Getting position...');
 }
 
 button.addEventListener('click', trackUserHandler);
+
+Promise.race([getPosition(), setTimer(1000)]).then(data => {
+  console.log(data);
+});
+
+// Promise.all([
+//   getPosition(),
+//   setTimer(1000)
+// ]).then(promiseData => {
+//   console.log(promiseData);
+// })
+
+Promise.allSettled([getPosition(),setTimer(1000)]).then(promiseData => {
+  console.log(promiseData);
+})
+
+// let result = 0;
+// for (let i = 0; i < 100000000; i++) {
+//   result += i;
+// }
+
+// console.log(result);
