b4826c1d2b3ce3ea59402bc79e3babc490e38a4e
Drasenberg <macoel14@gmail.com>
2023-03-06 06:46:35 +0100

    End of browser support

--- support-01-starting-setup/webpack.config.js
+++ support-01-starting-setup/webpack.config.js
@@ -9,7 +9,6 @@ module.exports = {
     path: path.resolve(__dirname, 'assets', 'scripts'),
     publicPath: 'assets/scripts/'
   },
-  devtool: 'cheap-module-eval-source-map',
   // devServer: {
   //   contentBase: './'
   // }
